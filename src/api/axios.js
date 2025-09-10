c407415199a159df7b67eefb472dd0df87c84b1f
Muqaddas-lab <muqaddasfatima200@gmail.com>
2025-09-09 22:11:17 -0700

    Update axios baseURL to deployed backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,26 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// // ✅ token send in each request 
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-commerce-backend-coral-ten.vercel.app/api", // ✅ deployed backend URL
 });
 
 // ✅ token send in each request 
